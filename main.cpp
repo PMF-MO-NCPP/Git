6aa9fd670d58a6dec71a352ca18d86106fb49a43
mjurak <jurak@math.hr>
2019-03-06 15:48:40 +0100

    Uniformna inicijalizacija.

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-   vector<int> vec(10, 3);
+   vector<int> vec{10, 3};
 
-    for(unsigned int i = 0; i < vec.size(); ++i)
+   for(unsigned int i = 0; i < vec.size(); ++i)
          cout << vec[i] << ",";
     cout << "\n";
-  std::cout << "Hello world!\n";
+    std::cout << "Hello world!\n";
 
   return 0;
 }

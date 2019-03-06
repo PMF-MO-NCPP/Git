6798685fd05e3c34b566e62ff6accddf0eef68a4
mjurak <jurak@math.hr>
2019-03-06 13:46:11 +0100

    Promjena

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-  
+   vector<int> vec(10, 3);
+
+    for(unsigned int i = 0; i < vec.size(); ++i)
+         cout << vec[i] << ",";
+    cout << "\n";
   std::cout << "Hello world!\n";
 
   return 0;

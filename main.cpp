af480f88b11e02de57e806e8f74593330c8ee99c
Mladen Jurak <jurak@pred-pr1.studenti.math.hr>
2025-09-29 18:18:57 +0200

    maknuo using std

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 int main()
 {
    int x =0;
    int y(0);
    int z{0};
-   vector<int> vec{16, 3, 5};
+   std::vector<int> vec{16, 3, 5};
 
    for(auto x : vec)
          cout << x << ",";
-    cout << "\n";
+    std::cout << "\n";
     std::cout << "Hello world!!!\n";
 
   return 0;
